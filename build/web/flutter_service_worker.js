d0210de24ef96275f8de7c7d01118111256eaa30
Ziad Wahida <ziad_wahida@hotmail.ch>
2025-03-26 00:47:44 +0100

    Build with correct base href for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "38619a270bef93c0b7b56a0cc64b9c44",
+const RESOURCES = {"flutter_bootstrap.js": "e30f53c3debf47e4b78e6c3255d3f007",
 "version.json": "5687018129c3d895921fa2bea4c69018",
-"index.html": "9bb0d4ccd9ec7238808cb8d3aeab7cd9",
-"/": "9bb0d4ccd9ec7238808cb8d3aeab7cd9",
+"index.html": "68f68c992e980261df02d786ce65746a",
+"/": "68f68c992e980261df02d786ce65746a",
 "main.dart.js": "ff6de64d63ee3a05d931781f3a9e3f68",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
