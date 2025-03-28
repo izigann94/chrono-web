7934086c6230f2265dc8e4709888c5553081f561
Ziad Wahida <ziad_wahida@hotmail.ch>
2025-03-28 14:12:00 +0100

    Final fix: relative paths working

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3752352711"
+    serviceWorkerVersion: "4229990008"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ffc79c16f332d4d71cde3a1b5994a897",
+const RESOURCES = {"flutter_bootstrap.js": "a857c40718b12ca42d5fadd7b72673b1",
 "version.json": "5687018129c3d895921fa2bea4c69018",
 "index.html": "9bb0d4ccd9ec7238808cb8d3aeab7cd9",
 "/": "9bb0d4ccd9ec7238808cb8d3aeab7cd9",
